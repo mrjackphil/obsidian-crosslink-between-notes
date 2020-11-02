ee9f97ed6acefcea27cb232499512c35e966b875
MrJackphil <geimerus@gmail.com>
2020-11-02 14:23:41 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Add links to current note
+This plugin adds a command which allows to add a link to the current note at the bottom of selected notes
+## How to use
+- Put a cursor at a line with existed [[WikiLinks]] or select content with [[WikiLinks]].
+- Open command palette (Ctrl+P) and find `Add links to current note: add links` command.
+    - You can set hotkey in `Settings->Hotkey` section
+- Execute command. It will search for wikilinks and add a `[[CURRENT_OPENED_NOTE_NAME]]` at the bottom of the each note.
+## Compatibility
+Tested on Obsidian 0.9.10
+## How to install
+You can install the plugin via the Community Plugins tab within Obsidian. Just search for "Add links to current note." when it'll be available.
+### Manual installation
+You can install the plugin manually by downloading the zip of the latest Github Release. Unzip the contents into your `<vault>/.obsidian/plugins/mrj-crosslink-between-notes` directory. [More info](https://forum.obsidian.md/t/plugins-mini-faq/7737).
+## Donation
+- [Patreon](https://patreon.com/mrjackphil)
+- [Paypal](https://www.paypal.com/paypalme/mrjackphil)
---
+++ main.ts
@@ -0,0 +1,68 @@
+import {
+	App,
+	EditableFileView,
+	MarkdownSourceView,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting, TFile
+} from 'obsidian';
+
+export default class MyPlugin extends Plugin {
+	onload() {
+		console.log('loading plugin');
+
+		const addBacklink = () => {
+			const currentView = this.app.workspace.activeLeaf.view
+
+			const fileName = currentView.getDisplayText()
+
+			if (!(currentView instanceof MarkdownView)) { return }
+
+			const cm = currentView.sourceMode.cmEditor
+			const cursor = cm.getCursor()
+            const selectedRange = cm.getSelections().join('\n')
+			const line = selectedRange || cm.getLine(cursor.line)
+
+			const links = line.match(/\[\[.+?\]\]/gi)
+			const files = this.app.vault.getFiles()
+
+			console.log(links)
+
+			let successed = [] as TFile[]
+
+			links.forEach(lnk => {
+				const lnkName = lnk
+					.replace(/(\[\[|\]\])/g, '')
+					.replace(/\|.+/, '')
+					.replace(/\#.+/, '')
+
+				const file = files.filter(e =>  e.name === lnkName
+					|| e.path === lnkName
+					|| e.basename === lnkName
+				)[0]
+
+				if (file) {
+					// @ts-ignore
+					this.app.vault.modify(file, file.cachedData + `\n[[${fileName}]]`)
+					successed.push(file)
+				}
+
+				new Notice(`Add link [[${fileName}]] to ${successed.map(e => e.basename).join(',')}`)
+			})
+		}
+
+		this.addCommand({
+		    id: 'add-link-to-current',
+		    name: 'add links',
+		    callback: addBacklink,
+		    hotkeys: []
+	    })
+	}
+
+	onunload() {
+		console.log('unloading plugin');
+	}
+}
