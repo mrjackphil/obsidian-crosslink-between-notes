8c582d27adf996db09c288356493f96bacadcfad
MrJackphil <geimerus@gmail.com>
2021-04-20 09:55:21 +0300

    Add template functionality

--- main.ts
+++ main.ts
@@ -1,13 +1,28 @@
 import {
+    App,
     MarkdownView,
     Notice,
-    Plugin,
+    Plugin, PluginSettingTab, Setting,
     TFile
 } from 'obsidian';
 
+interface PluginSettings {
+    template: string
+}
+
+const DEFAULT_SETTINGS: PluginSettings = {
+    template: '- $link'
+}
+
 export default class AddLinkToCurrentNotePlugin extends Plugin {
-    onload() {
+    settings: PluginSettings;
+
+    async onload() {
         console.log('loading plugin');
+
+        await this.loadSettings();
+
+        this.addSettingTab(new CrosslinkSettingsTab(this.app, this));
 
         const addBacklink = () => {
             const currentView = this.app.workspace.activeLeaf.view
@@ -30,58 +45,34 @@ export default class AddLinkToCurrentNotePlugin extends Plugin {
 
             const linksWiki = line.match(regexpWiki) || []
             const linksMD = line.match(regexpMD) || []
-            const files = this.app.vault.getFiles()
 
             let succeed = [] as TFile[]
 
             const currentFileLink = this.app.fileManager.generateMarkdownLink(currentFile, currentFile.path)
+            const lineToPaste = this.settings.template.replace('$link', currentFileLink)
 
-            linksWiki.forEach(async (lnk) => {
-                const lnkName = lnk
+            const ar = [linksWiki, linksMD]
+            ar.flat().forEach(async (lnk) => {
+                const wikiName = lnk
                     .replace(/(\[\[|]])/g, '')
                     .replace(/\|.+/, '')
                     .replace(/#.+/, '')
 
-                const file = files.filter(e => e.name === lnkName
-                    || e.path === lnkName
-                    || e.basename === lnkName
-                )[0]
-
-                if (!file) {
-                    return
-                }
-
-                if (file) {
-                    const {vault} = this.app
-
-                    const data = await vault.read(file)
-                    await vault.modify(file, data + `\n` + currentFileLink)
-                    succeed.push(file)
-                }
-
-                new Notice(`Add link [[${fileName}]] to ${succeed.map(e => e.basename).join(',')}`)
-            })
-
-            linksMD.forEach(async (lnk) => {
-                const lnkName = lnk.match(/\(.+?\)/)[0]
+                const mdName = lnk.match(/\(.+?\)/)[0]
                     ?.replace('.md', '')
                     ?.replace(/[()]/g, '')
 
-                const file = files.filter(e => e.name === lnkName
-                    || e.path === lnkName
-                    || e.basename === lnkName
-                )[0]
+                const file = this.getFilesByName(wikiName) || this.getFilesByName(mdName)
 
                 if (!file) {
                     return
                 }
 
-
                 if (file) {
                     const {vault} = this.app
 
                     const data = await vault.read(file)
-                    await vault.modify(file, data + `\n` + '- ' + currentFileLink)
+                    await vault.modify(file, data + `\n` + lineToPaste)
                     succeed.push(file)
                 }
 
@@ -99,5 +90,49 @@ export default class AddLinkToCurrentNotePlugin extends Plugin {
 
     onunload() {
         console.log('unloading plugin');
+    }
+
+    getFilesByName(name: string) {
+        const files = this.app.vault.getFiles()
+
+        return files.filter(e => e.name === name
+            || e.path === name
+            || e.basename === name
+        )[0]
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+}
+
+class CrosslinkSettingsTab extends PluginSettingTab {
+    plugin: AddLinkToCurrentNotePlugin;
+
+    constructor(app: App, plugin: AddLinkToCurrentNotePlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        let {containerEl} = this;
+
+        containerEl.empty();
+
+        containerEl.createEl('h2', {text: 'Settings for "Add links to the current note" plugin'});
+
+        new Setting(containerEl)
+            .setName('Template')
+            .setDesc('How the link will be pasted. `$link` will be replaced with link itself.')
+            .addText(text => text
+                .setValue(this.plugin.settings.template)
+                .onChange(async (value) => {
+                    this.plugin.settings.template = value;
+                    await this.plugin.saveSettings();
+                }));
     }
 }
