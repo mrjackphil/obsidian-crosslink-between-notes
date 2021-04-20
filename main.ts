b407ee255bd9ad18d9a9b1a38a81ab28f5eec2db
MrJackphil <geimerus@gmail.com>
2021-04-20 09:36:41 +0300

    Recognize MD and Wikilinks. Add MD or Wiki links based on settings. Add as list item by default

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import {
     TFile
 } from 'obsidian';
 
-export default class MyPlugin extends Plugin {
+export default class AddLinkToCurrentNotePlugin extends Plugin {
     onload() {
         console.log('loading plugin');
 
@@ -18,17 +18,25 @@ export default class MyPlugin extends Plugin {
                 return
             }
 
-            const cm = currentView.sourceMode.cmEditor
+            const currentFile = currentView.file
+
+            const cm = currentView.editor
             const cursor = cm.getCursor()
-            const selectedRange = cm.getSelections().join('\n')
+            const selectedRange = cm.getSelection()
             const line = selectedRange || cm.getLine(cursor.line)
 
-            const links = line.match(/\[\[.+?]]/gi)
+            const regexpMD = /(\[.+])\([\w\s,.\-%$]+\)/gi
+            const regexpWiki = /\[\[.+]]/gi
+
+            const linksWiki = line.match(regexpWiki) || []
+            const linksMD = line.match(regexpMD) || []
             const files = this.app.vault.getFiles()
 
             let succeed = [] as TFile[]
 
-            links.forEach(async (lnk) => {
+            const currentFileLink = this.app.fileManager.generateMarkdownLink(currentFile, currentFile.path)
+
+            linksWiki.forEach(async (lnk) => {
                 const lnkName = lnk
                     .replace(/(\[\[|]])/g, '')
                     .replace(/\|.+/, '')
@@ -39,11 +47,41 @@ export default class MyPlugin extends Plugin {
                     || e.basename === lnkName
                 )[0]
 
+                if (!file) {
+                    return
+                }
+
                 if (file) {
                     const {vault} = this.app
 
                     const data = await vault.read(file)
-                    await vault.modify(file, data + `\n[[${fileName}]]`)
+                    await vault.modify(file, data + `\n` + currentFileLink)
+                    succeed.push(file)
+                }
+
+                new Notice(`Add link [[${fileName}]] to ${succeed.map(e => e.basename).join(',')}`)
+            })
+
+            linksMD.forEach(async (lnk) => {
+                const lnkName = lnk.match(/\(.+?\)/)[0]
+                    ?.replace('.md', '')
+                    ?.replace(/[()]/g, '')
+
+                const file = files.filter(e => e.name === lnkName
+                    || e.path === lnkName
+                    || e.basename === lnkName
+                )[0]
+
+                if (!file) {
+                    return
+                }
+
+
+                if (file) {
+                    const {vault} = this.app
+
+                    const data = await vault.read(file)
+                    await vault.modify(file, data + `\n` + '- ' + currentFileLink)
                     succeed.push(file)
                 }
 
